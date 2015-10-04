1c43bfecf1a8b2f038a2cbcbdd899ed1a57ab377
Aidan Li <lidanai@berkeley.edu>
2015-10-03 20:58:25 -0700

    added admin management for users

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Event
+
+class EventInline(admin.TabularInline):
+	model = Event
+	extra = 0
+
+class UserAdmin(admin.ModelAdmin):
+	fields = ['name_last', 'name_first', 'gamer_tag']
+	list_display = ('name_last', 'name_first', 'gamer_tag')
+	inlines = [EventInline]
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Event)
