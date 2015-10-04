245f3de7e2a5044840030e68769cd2238e3dc008
Elbert Lin <elbertlin168@berkeley.edu>
2015-10-03 20:45:27 -0700

    Adding str to models, some framework in views

--- web/models.py
+++ web/models.py
@@ -5,11 +5,15 @@ from django.utils import timezone
 # Create your models here.
 
 class User(models.Model):
-	name = models.CharField(max_length=50)
-	gamer_tag = models.CharField(max_length=50, blank=True, null=True)
+	name_first = models.CharField(max_length=50)
+	name_last = models.CharField(max_length=50)
+	gamer_tag = models.CharField(max_length=50, blank=True)
 
 	events = models.ForeignKey(Event) # many users may attend an event
 	friends = models.ForeignKey('self') # for future moves to make friends list
+
+	def __str__(self):
+		return self.name_first + ' ' + name_last + '/' + self.gamer_tag
 
 class Event(models.Model):
 	host = models.ForeignKey(User) # many events may be hosted by a user
@@ -17,4 +21,7 @@ class Event(models.Model):
 	date = models.DateTimeField('Date of event')
 	capacity = models.IntegerField('Capacity of event', default=0)
 	location = models.CharField(max_length=200)
-	description = models.CharField(max_length=200)
+	description = models.CharField(max_length=200, blank=True)
+
+	def __str__(self):
+		return str(self.host) + ':' + str(self.date)
